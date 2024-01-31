2d54414c4a08169e76957e379f27030e0e29765f
IvonneConde <ivonneconde91@gmail.com>
2024-01-31 14:38:38 -0600

    Information completed at 80%

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Soy programadora web y lo aprendí en Tecnolochicas.')
+  .typeString('Explorer of the digital world, building stories through code')
   .pauseFor(200)
   .deleteChars(10)
   .start();
